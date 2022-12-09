f4c0549077dd68b51bfd4bdaf59102d42aa9eb44
Discipl4 <gghssa1@gmail.com>
2022-12-10 00:06:52 +0700

    updated

--- reviews/reviews.ino
+++ reviews/reviews.ino
@@ -3,26 +3,30 @@
 #include <LiquidCrystal_I2C.h>
 #include <WiFi.h>
 
-//String ssid = "Pak DELUCI";
-//String passwd = "hallo123456";
+String ssid = "Pak DELUCI";
+String passwd = "hallo123456";
 
 //String ssid = "iPhone 8";
 //String passwd = "321123321";
 
-String ssid = "Indah ";
-String passwd = "aldo123456";
+//String ssid = "Indah ";
+//String passwd = "aldo123456";
+
+//String ssid = "UDINUS-H4";
+//String passwd = "";
 
 HTTPClient http;
 
 //String url = "http://192.168.137.152:3000/api/v1/review/now";
 //String url = "http://172.20.10.2:3000/api/v1/review/now";
-String url = "https://3000-fahmibahtia-arduinorevi-ep2bm3kfyxh.ws-us77.gitpod.io/api/v1/review/now";
+//String url = "https://3000-fahmibahtia-arduinorevi-g19mj5r9ta1.ws-us77.gitpod.io/api/v1/review/now";
+String url = "http://192.168.169.251:3000/api/v1/review/now";
 
 void connectWifi();
 void postPuas();
 void postBiasa();
 void postTidakPuas();
-//void scrollText();
+void scrollText();
 
 #define PIN_PUAS 2
 #define PIN_BIASA 16
@@ -34,9 +38,6 @@ int lcdRows = 2;
 
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
 
-//String messageStatic = "ARDUINO";
-String messageToScroll = "INI TEXT JALAN";
-
 void setup() {
 
   Serial.begin(115200);
@@ -45,20 +46,15 @@ void setup() {
   pinMode(PIN_BIASA, INPUT_PULLUP);
   pinMode(PIN_TIDAK_PUAS, INPUT_PULLUP);
   pinMode(PIN_LED, OUTPUT);
+  
+  lcd.init();
+  lcd.backlight();
 
   connectWifi();
+
   
-//  lcd.init();
-//  lcd.backlight();
 
-  int puasValue = getPuas();
-  int biasaValue = getBiasa();
-  int tidakPuasValue = getTidakPuas();
-
-  String strPuasValue;
-  strPuasValue += puasValue;
-
-  Serial.println(strPuasValue);
+//  Serial.println(strPuasValue);
 
 //  Serial.println(puasValue);
 
@@ -70,26 +66,54 @@ void setup() {
 
 void loop() {
 
+  int puasValue = getPuas();
+  int biasaValue = getBiasa();
+  int tidakPuasValue = getTidakPuas();
+
+  puasValue += 1;
+  biasaValue += 1;
+  tidakPuasValue += 1;
+
+  String strPuasValue;
+  strPuasValue += puasValue;
+
+  String strBiasaValue;
+  strBiasaValue += biasaValue;
+
+  String strTidakPuasValue;
+  strTidakPuasValue += tidakPuasValue;
+  
   while (digitalRead(PIN_PUAS) != LOW && digitalRead(PIN_BIASA) != LOW && digitalRead(PIN_TIDAK_PUAS) != LOW) {
-//    scrollText(1, messageToScroll, 100, lcdColumns);
-    digitalWrite(PIN_LED, 1);
+//    digitalWrite(PIN_LED, 1);
   }
   
   if (digitalRead(PIN_PUAS) == LOW) {
+
+    lcd.clear();
     
     postPuas();
+
+    lcd.println("NOICE : " + strPuasValue);
     
   }
 
   if (digitalRead(PIN_BIASA) == LOW) {
+
+    lcd.clear();
     
     postBiasa();
 
-  }
+    lcd.println("MEH : " + strBiasaValue);
 
+  }
+  
   if (digitalRead(PIN_TIDAK_PUAS) == LOW) {
+
+    lcd.clear();
     
     postTidakPuas();
+
+    lcd.println("SADGE : " + strTidakPuasValue);
     
   }
 
@@ -103,17 +127,31 @@ void connectWifi() {
   
   WiFi.begin(ssid.c_str(),passwd.c_str());
 
+  Serial.println("Connecting to WiFi....");
+
   while (WiFi.status() != WL_CONNECTED) {
-    
-    delay(5000);
-    Serial.println("Connecting to WiFi...");
+    scrollText(0, "Connecting to WiFi....", 100, lcdColumns);
+    scrollText(1, "Connecting to WiFi....", 100, lcdColumns);
   }
 
   if (WiFi.status() == WL_CONNECTED) {
     
-    delay(3000);
+    delay(1000);
     Serial.print("Connected to ");
     Serial.println(ssid);
+
+    for (int i = 0; i < 2; i++) {
+      lcd.setCursor(0, 0);
+      lcd.print("    CONNECTED");
+      delay(700);
+      lcd.clear();
+      delay(500);
+      lcd.setCursor(0, 1);
+      lcd.print("    CONNECTED");
+      delay(700);
+      lcd.clear();
+      delay(500);
+    }
     
     delay(1000);
     Serial.println("Ready to go");
@@ -307,14 +345,14 @@ int getTidakPuas() {
   
 }
 
-//void scrollText(int row, String message, int delayTime, int lcdColumns) {
-//  for (int i=0; i < lcdColumns; i++) {
-//    message = " " + message;  
-//  } 
-//  message = message + " "; 
-//  for (int pos = 0; pos < message.length(); pos++) {
-//    lcd.setCursor(0, row);
-//    lcd.print(message.substring(pos, pos + lcdColumns));
-//    delay(delayTime);
-//  }
-//}
+void scrollText(int row, String message, int delayTime, int lcdColumns) {
+  for (int i=0; i < lcdColumns; i++) {
+    message = " " + message;  
+  } 
+  message = message + " "; 
+  for (int pos = 0; pos < message.length(); pos++) {
+    lcd.setCursor(0, row);
+    lcd.print(message.substring(pos, pos + lcdColumns));
+    delay(delayTime);
+  }
+}
